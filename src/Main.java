571a6a3d3a555bff5b5928be6ebeba034c0fd9fd
kuvii <kuvilet@gmail.com>
2023-01-17 12:06:32 +0000

    Experimento Uno ✔

--- src/Main.java
+++ src/Main.java
@@ -4,11 +4,13 @@ import Clases.Consumer;
 
 public class Main {
 
+    // Experimento Uno
+
     //Orchard
     static int MAX_SPACES = 15;
 
     //Productor
-    static int FARMER_PRODUCTION = 15;
+    static int FARMER_PRODUCTION = 5;
 
     //Consumer
     static int MAX_GROW_TIME = 4;
@@ -19,9 +21,9 @@ public class Main {
         Orchard orchard = new Orchard(MAX_SPACES);
         Productor productor = new Productor("Adrian", FARMER_PRODUCTION, MAX_GROW_TIME, orchard);
         Consumer consumer1 = new Consumer("Aday", MAX_TO_CONSUME, 3, orchard);
-        Consumer consumer2 = new Consumer("Daniel", MAX_TO_CONSUME, 8, orchard);
+
         productor.start();
         consumer1.start();
-        consumer2.start();
+
     }
 }
