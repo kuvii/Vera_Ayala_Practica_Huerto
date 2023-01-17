504be8b67cbb4ebb4641d773f9a76c129c0a0b9a
kuvii <kuvilet@gmail.com>
2023-01-17 12:24:13 +0000

    Experimento Tres ✔

--- src/Main.java
+++ src/Main.java
@@ -8,20 +8,26 @@ public class Main {
     static int MAX_SPACES = 15;
 
     //Productor
-    static int FARMER_PRODUCTION = 15;
+    static int FARMER_PRODUCTION = 5;
 
     //Consumer
     static int MAX_GROW_TIME = 4;
     static int MAX_TO_CONSUME = 5;
 
+    static int LOW_PRIORITY = 0;
+    static int MID_PRIORITY = 5;
+    static int MAX_PRIORITY = 10;
+
     public static void main(String[] args) {
 
         Orchard orchard = new Orchard(MAX_SPACES);
         Productor productor = new Productor("Adrian", FARMER_PRODUCTION, MAX_GROW_TIME, orchard);
-        Consumer consumer1 = new Consumer("Aday", MAX_TO_CONSUME, 3, orchard);
-        Consumer consumer2 = new Consumer("Daniel", MAX_TO_CONSUME, 8, orchard);
+        Consumer consumer = new Consumer("Aday", MAX_TO_CONSUME, 3, orchard);
+
+        productor.setPriority(MAX_PRIORITY);
+        consumer.setPriority(MID_PRIORITY);
+
         productor.start();
-        consumer1.start();
-        consumer2.start();
+        consumer.start();
     }
 }
