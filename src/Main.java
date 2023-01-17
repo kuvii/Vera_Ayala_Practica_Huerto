3b4da9157344d103a1f7bcb4a4c85250fdc698ac
kuvii <kuvilet@gmail.com>
2023-01-17 12:15:51 +0000

    Experimento Dos ✔

--- src/Main.java
+++ src/Main.java
@@ -4,23 +4,24 @@ import Clases.Consumer;
 
 public class Main {
 
-    // Experimento Uno
+    // Experimento Dos
 
     //Orchard
     static int MAX_SPACES = 15;
 
     //Productor
     static int FARMER_PRODUCTION = 5;
+    static int MAX_GROW_TIME = 2;
 
     //Consumer
-    static int MAX_GROW_TIME = 4;
     static int MAX_TO_CONSUME = 5;
+    static int CONSUME_TIME = 5;
 
     public static void main(String[] args) {
 
         Orchard orchard = new Orchard(MAX_SPACES);
         Productor productor = new Productor("Adrian", FARMER_PRODUCTION, MAX_GROW_TIME, orchard);
-        Consumer consumer1 = new Consumer("Aday", MAX_TO_CONSUME, 3, orchard);
+        Consumer consumer1 = new Consumer("Aday", MAX_TO_CONSUME, CONSUME_TIME, orchard);
 
         productor.start();
         consumer1.start();
